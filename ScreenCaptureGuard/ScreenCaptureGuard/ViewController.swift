49e615062e446346da6dc16f8e19d2c944defcc2
Agatai Embeev <beshssg@gmail.com>
2024-02-25 17:45:36 +0600

    PROJECT: Инициализация проекта

---
+++ ScreenCaptureGuard/ScreenCaptureGuard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenCaptureGuard
+//
+//  Created by Agatai Embeev on 25.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
