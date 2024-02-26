bc75180e720d573ac13103fdd01c6ce81daf893c
Agatai Embeev <beshssg@gmail.com>
2024-02-26 11:48:30 +0600

    FEA: Перенос логики роотового вью

--- ScreenCaptureGuard/ScreenCaptureGuard/Sources/App/AppDelegate.swift
+++ ScreenCaptureGuard/ScreenCaptureGuard/Sources/App/AppDelegate.swift
@@ -13,9 +13,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        setupRootView()
+        return true
+    }
+}
+
+private extension AppDelegate {
+    
+    func setupRootView() {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = ViewController()
         window?.makeKeyAndVisible()
-        return true
     }
 }
